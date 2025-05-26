ba7ff28e3a58dcc4bfb9d350d537e72f8539f329
AxlPaniagua <axelpaniaguab54@gmail.com>
2025-05-26 01:08:33 -0600

    (add): cambios en el blade

--- resources/views/admin/dashboardMain.blade.php
+++ resources/views/admin/dashboardMain.blade.php
@@ -9,25 +9,21 @@
 @section('content')
     <div class="row">
         <!-- Total Clientes -->
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <x-adminlte-info-box title="Clientes" text="{{ $totalClientes }}" icon="fas fa-users" theme="info"/>
+            <a href="{{ route('admin.clientes.index') }}" class="btn btn-info btn-block mt-2">Ver Clientes</a>
         </div>
 
         <!-- Total Reservas -->
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <x-adminlte-info-box title="Reservas" text="{{ $totalReservas }}" icon="fas fa-calendar-check" theme="success"/>
+            <a href="{{ route('admin.reservas.index') }}" class="btn btn-success btn-block mt-2">Ver Reservas</a>
         </div>
 
         <!-- Total Tours -->
-        <div class="col-md-4">
+        <div class="col-md-4 mb-3">
             <x-adminlte-info-box title="Tours" text="{{ $totalTours }}" icon="fas fa-map" theme="warning"/>
+            <a href="{{ route('admin.tours.index') }}" class="btn btn-warning btn-block mt-2">Ver Tours</a>
         </div>
-    </div>
-
-    <div class="mt-4">
-        <a href="{{ route('admin.reservas.index') }}" class="btn btn-primary">Ver Reservas</a>
-        <a href="{{ route('admin.clientes.index') }}" class="btn btn-secondary">Ver Clientes</a>
-        <a href="{{ route('admin.tours.index') }}" class="btn btn-success">Ver Tours</a>
-        
     </div>
 @stop
