b4d64d1d90e2a559c7216faa0464be321872ce11
AxlPaniagua <axelpaniaguab54@gmail.com>
2025-06-02 12:23:14 -0600

    (add): add info to the main dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,18 +5,18 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Reserva;
-use App\Models\Cliente;
 use App\Models\Tour;
 use App\Models\Role;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function index(){
     $totalReservas = Reserva::count();
-    $totalClientes = Cliente::count();
+    $totalUsuarios = User::count();
     $totalTours = Tour::count();
     $roles = Role::count();
-
-    return view('admin.dashboardMain', compact('totalClientes', 'totalReservas', 'totalTours'));
+    
+    return view('admin.dashboardMain', compact( 'totalReservas', 'totalTours','totalUsuarios'));
 }
 }
--- resources/views/admin/dashboardMain.blade.php
+++ resources/views/admin/dashboardMain.blade.php
@@ -10,8 +10,8 @@
     <div class="row">
         <!-- Total Clientes -->
         <div class="col-md-4 mb-3">
-            <x-adminlte-info-box title="Clientes" text="{{ $totalClientes }}" icon="fas fa-users" theme="info"/>
-            <a href="{{ route('admin.clientes.index') }}" class="btn btn-info btn-block mt-2">Ver Clientes</a>
+            <x-adminlte-info-box title="Usuarios" text="{{ $totalUsuarios }}" icon="fas fa-users" theme="info"/>
+            <a href="{{ route('admin.users.index') }}" class="btn btn-info btn-block mt-2">Ver Usuarios</a>
         </div>
 
         <!-- Total Reservas -->
