5186302eb911547d137d4bb9022a10be167e5c69
AxlPaniagua <axelpaniaguab54@gmail.com>
2025-06-10 20:16:06 -0600

    (feat): add flags for the traslation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,12 +26,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
 <body>
-<div class="text-end p-2">
-    <a href="{{ route('cambiar.idioma', 'es') }}">ES</a> |
-    <a href="{{ route('cambiar.idioma', 'en') }}">EN</a> |
-    <a href="{{ route('cambiar.idioma', 'fr') }}">FR</a>
-</div>
-
 <nav class="navbar-custom">
     <div class="navbar-logo">
         <img src="{{ asset('images/logoCompanyWhite.png') }}" alt="Green Vacations" style="height: 70px; margin-right: 30px;">
@@ -76,6 +70,40 @@
                 <i class="fas fa-toolbox"></i> Admin
             </a>
         @endif
+        {{-- Dropdown de idiomas --}}
+        <div class="dropdown">
+            <button class="btn btn-sm btn-outline-light dropdown-toggle d-flex align-items-center gap-2" type="button" id="languageDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+                {{-- Mostrar bandera actual --}}
+                @php
+                    $flag = match(app()->getLocale()) {
+                        'es' => 'es.png',
+                        'en' => 'gb.png',
+                        'fr' => 'fr.png',
+                        default => 'es.png'
+                    };
+                @endphp
+                <img src="{{ asset('images/' . $flag) }}" alt="Idioma actual" width="20" height="15">
+                {{ strtoupper(app()->getLocale()) }}
+            </button>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="languageDropdown">
+                <li>
+                    <a class="dropdown-item d-flex align-items-center gap-2" href="{{ route('cambiar.idioma', 'es') }}">
+                        <img src="{{ asset('images/es.png') }}" width="20" height="15"> Español
+                    </a>
+                </li>
+                <li>
+                    <a class="dropdown-item d-flex align-items-center gap-2" href="{{ route('cambiar.idioma', 'en') }}">
+                        <img src="{{ asset('images/gb.png') }}" width="20" height="15"> English
+                    </a>
+                </li>
+                <li>
+                    <a class="dropdown-item d-flex align-items-center gap-2" href="{{ route('cambiar.idioma', 'fr') }}">
+                        <img src="{{ asset('images/fr.png') }}" width="20" height="15"> Français
+                    </a>
+                </li>
+            </ul>
+        </div>
+
     </div>
 </nav>
 
