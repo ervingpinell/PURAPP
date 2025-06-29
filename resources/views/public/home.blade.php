a339d1d8d0119bc55a1df010546b0f480ccbf082
AxlPaniagua <axelpaniaguab54@gmail.com>
2025-06-28 18:11:14 -0600

    se cambia los iconos del dropdown de el perfil

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -85,31 +85,51 @@
     <div class="navbar-actions">
         <i class="fas fa-shopping-cart" title="{{ __('adminlte::adminlte.cart') }}"></i>
         @auth
-            <div class="dropdown">
-                <a class="nav-link dropdown-toggle text-white" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <div class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="userDropdown" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="fas fa-user-circle"></i> {{ Auth::user()->full_name }}
                 </a>
-                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                <ul class="dropdown-menu dropdown-menu-end shadow-sm" aria-labelledby="userDropdown" style="min-width:180px;">
                     @if(Auth::user()->role_id === 3)
-                        <li><a class="dropdown-item" href="{{ route('my-reservations') }}">{{ __('adminlte::adminlte.my_reservations') }}</a></li>
-                    @endif
-                    <li><a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('adminlte::adminlte.profile') }}</a></li>
                     <li>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-inline">
-                            @csrf
-                            <button type="submit" class="dropdown-item">{{ __('adminlte::adminlte.logout') }}</button>
-                        </form>
+                        <a class="dropdown-item d-flex align-items-center" href="{{ route('my-reservations') }}">
+                        <i class="fas fa-calendar-check me-2 text-success"></i>
+                        {{ __('My Reservations') }}
+                        </a>
+                    </li>
+                    @endif
+
+                    <li>
+                    <a class="dropdown-item d-flex align-items-center" href="{{ route('profile.edit') }}">
+                        <i class="fas fa-id-card-alt me-2 text-primary"></i>
+                        {{ __('Profile') }}
+                    </a>
+                    </li>
+
+                    <li><hr class="dropdown-divider"></li>
+
+                    <li>
+                    <form action="{{ route('logout') }}" method="POST" class="d-inline w-100">
+                        @csrf
+                        <button type="submit" class="dropdown-item d-flex align-items-center text-danger">
+                        <i class="fas fa-sign-out-alt me-2"></i>
+                        {{ __('Logout') }}
+                        </button>
+                    </form>
                     </li>
                 </ul>
             </div>
         @else
-            <a href="{{ route('login') }}">
+            <a href="{{ route('login') }}" class="text-white">
                 <i class="fas fa-user" title="Login"></i>
             </a>
         @endauth
 
-        @if(Auth::check() && in_array(Auth::user()->role_id, [1, 2]))
-            <a href="{{ route('admin.home') }}" class="btn btn-outline-light btn-sm d-flex align-items-center gap-2" style="border-radius: 25px;">
+        @if(Auth::check() && in_array(Auth::user()->role_id, [1, 2])) {{-- Assuming roles 1 and 2 are for admin --}}
+            <a href="{{ route('admin.home') }}"
+               class="btn btn-outline-light btn-sm d-flex align-items-center gap-2"
+               style="border-radius: 25px;">
                 <i class="fas fa-toolbox"></i> Admin
             </a>
         @endif
