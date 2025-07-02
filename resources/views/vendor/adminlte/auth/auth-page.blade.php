50b4a615302a5e06391b2e8ead9b41cdf180d2cd
ervingpinell <ervingpinell@gmail.com>
2025-07-02 11:23:29 -0600

    Add gv.css for dark mode styles and layout adjustments

--- resources/views/vendor/adminlte/auth/auth-page.blade.php
+++ resources/views/vendor/adminlte/auth/auth-page.blade.php
@@ -96,5 +96,5 @@
     @yield('js')
 
     {{-- Bootstrap JS para dropdowns --}}
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 @stop
