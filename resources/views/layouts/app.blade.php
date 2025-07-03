942ae88787a5f4f9409780b71cc11ef6acae06df
ervingpinell <ervingpinell@gmail.com>
2025-07-02 21:09:31 -0600

    Refactor navbar styles for improved responsiveness and layout; update app layout to use Vite for CSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,6 @@
     <title>Green Vacations</title>
     <link rel="icon" type="image/png" href="{{ asset('images/logoCompanyWhite.png') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/home.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     @vite(['resources/css/home.css', 'resources/js/public.js'])
     
