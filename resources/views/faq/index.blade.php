7691bd499dfbc7a8c5db8c650197d4b16b274b57
Erving Pinell <ervingpinell@gmail.com>
2025-08-14 14:31:39 -0600

    refactor: Simplify FAQ accordion IDs for unique identification

--- resources/views/faq/index.blade.php
+++ resources/views/faq/index.blade.php
@@ -11,21 +11,34 @@
     @else
         <div class="accordion" id="faqAccordion">
             @foreach ($faqs as $faq)
+                @php
+                    // Asegura IDs únicos incluso si esta vista se incluye varias veces
+                    $uid = 'faq-'.($faq->id ?? $loop->index);
+                    $headingId = "heading-{$uid}";
+                    $collapseId = "collapse-{$uid}";
+                @endphp
+
                 <div class="accordion-item mb-2">
-                    <h2 class="accordion-header" id="heading{{ $faq->id }}">
-                        <button class="accordion-button collapsed" type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#collapse{{ $faq->id }}"
-                                aria-expanded="false"
-                                aria-controls="collapse{{ $faq->id }}">
+                    <h2 class="accordion-header" id="{{ $headingId }}">
+                        <button
+                            class="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#{{ $collapseId }}"
+                            aria-expanded="false"
+                            aria-controls="{{ $collapseId }}"
+                        >
                             <i class="fas fa-question-circle text-primary me-2"></i>
                             {{ $faq->translate()?->question ?? $faq->question }}
                         </button>
                     </h2>
-                    <div id="collapse{{ $faq->id }}"
-                         class="accordion-collapse collapse"
-                         aria-labelledby="heading{{ $faq->id }}"
-                         data-bs-parent="#faqAccordion">
+
+                    <div
+                        id="{{ $collapseId }}"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="{{ $headingId }}"
+                        data-bs-parent="#faqAccordion"
+                    >
                         <div class="accordion-body">
                             <span class="text-success me-2">✓</span>
                             <span>{!! nl2br(e($faq->translate()?->answer ?? $faq->answer)) !!}</span>
@@ -36,6 +49,7 @@
         </div>
     @endif
 </div>
+
 {{-- ✅ Modal de WhatsApp --}}
 @include('partials.ws-widget')
 @endsection
