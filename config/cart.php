910eb5af3d55beed9de15538ddc711f2cbf232c5
AxlPaniagua <axelpaniaguab54@gmail.com>
2025-09-18 12:17:36 -0600

    changes on the timer

--- config/cart.php
+++ config/cart.php
@@ -2,8 +2,8 @@
 
 return [
     // duración total del carrito
-    'expiry_minutes' => 40, // o 60 para 1 hora
+    'expiry_minutes' => 30, // o 60 para 1 hora
 
     // minutos que añade el botón “Extender”
-    'extend_minutes' => 40, // o 60
+    'extend_minutes' => 30, // o 60
 ];
